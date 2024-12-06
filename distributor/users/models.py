4caafc97fa7832d67a7dd855d236ca1ed83f9e2c
daniels-py <danielramirezs5518@gmail.com>
2024-12-06 18:01:55 -0500

    update model users

--- distributor/users/models.py
+++ distributor/users/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
 class CustomUser(AbstractUser):
     ROLE_CHOICES = (
         ('admin', 'Administrador'),
         ('employee', 'Empleado'),
+        ('common_user', 'Usuario Común'),  # Rol adicional para usuarios comunes
     )
-    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='employee')
+    role = models.CharField(max_length=15, choices=ROLE_CHOICES, default='common_user')
 
     def __str__(self):
         return self.username
+
+
+
