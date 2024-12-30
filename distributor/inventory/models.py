e60743dd2cd2dd53df5be07fc569bfaf8485dcc5
daniels-py <danielramirezs5518@gmail.com>
2024-12-30 17:17:44 -0500

    update en modelo inventario para consumo de apis

--- distributor/inventory/models.py
+++ distributor/inventory/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from products.models import Producto  # Asegúrate de importar correctamente el modelo Producto
-
+from django.utils.timezone import now
 
 class Inventario(models.Model):
-    producto = models.OneToOneField(Producto, on_delete=models.CASCADE, related_name='inventario')
-    unidades = models.PositiveIntegerField(default=0, verbose_name='Unidades')
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE, related_name="inventarios")
+    unidades = models.PositiveIntegerField()
+    fecha_ingreso = models.DateField(default=now, verbose_name="Fecha de Ingreso")  # Cambiado
 
     def __str__(self):
-        return f'{self.producto.nombre} ({self.producto.marca.nombre}) - {self.unidades} unidades'
+        return f"{self.producto.nombre} - Unidades: {self.unidades}"
 
     class Meta:
         verbose_name = 'Inventario'
--- distributor/inventory/serializers.py
+++ distributor/inventory/serializers.py
@@ -2,10 +2,10 @@ from rest_framework import serializers
 from .models import Inventario
 
 class InventarioSerializer(serializers.ModelSerializer):
-    producto_nombre = serializers.CharField(source='producto.nombre')
-    producto_marca = serializers.CharField(source='producto.marca.nombre')
-    producto_categoria = serializers.CharField(source='producto.categoria.nombre')
+    producto_nombre = serializers.CharField(source='producto.nombre', read_only=True)
+    producto_marca = serializers.CharField(source='producto.marca.nombre', read_only=True)
+    producto_categoria = serializers.CharField(source='producto.categoria.nombre', read_only=True)
 
     class Meta:
         model = Inventario
-        fields = ['id', 'producto', 'producto_nombre', 'producto_marca', 'producto_categoria', 'unidades']
+        fields = ['id', 'producto', 'producto_nombre', 'producto_marca', 'producto_categoria', 'unidades', 'fecha_ingreso']
