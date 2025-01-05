17bb39d6b01b9b15f09cc7456037bc98d32efff9
daniels-py <danielramirezs5518@gmail.com>
2025-01-04 20:30:20 -0500

    update en punto de venta para pruebas

--- distributor/sales/models.py
+++ distributor/sales/models.py
@@ -5,17 +5,18 @@ from products.models import Producto  # Asegúrate de importar correctamente el 
 
 class Venta(models.Model):
     fecha = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de la Venta")
-    total = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Total de la Venta")
+    total = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Total de la Venta", default=0.00)
     producto = models.ForeignKey(Producto, on_delete=models.CASCADE, related_name="ventas")
     cantidad = models.PositiveIntegerField(verbose_name="Cantidad Vendida")
-    # Podrías agregar más campos, como el cliente, el estado de la venta, etc.
 
     def save(self, *args, **kwargs):
-        # Actualizar el inventario al registrar la venta
+        # Calcular el total de la venta basado en el precio y la cantidad
+        self.total = self.producto.precio * self.cantidad
+
+        # También puedes actualizar el inventario aquí, como lo hiciste antes
         try:
             inventario_producto = Inventario.objects.get(producto=self.producto)
             if inventario_producto.unidades >= self.cantidad:
-                # Si hay suficientes unidades, se descuentan del inventario
                 inventario_producto.unidades -= self.cantidad
                 inventario_producto.save()
             else:
--- distributor/sales/serializers.py
+++ distributor/sales/serializers.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
 from .models import Venta
 
+
 class VentaSerializer(serializers.ModelSerializer):
+    total = serializers.DecimalField(max_digits=10, decimal_places=2, read_only=True)
+
     class Meta:
         model = Venta
         fields = ['id', 'fecha', 'producto', 'cantidad', 'total']
-
-
