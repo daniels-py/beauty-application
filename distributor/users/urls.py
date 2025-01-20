cb9750187e1ec765550e5a87188221405c09339c
daniels-py <danielramirezs5518@gmail.com>
2025-01-20 14:08:05 -0500

    update en controlador de usuarios

--- distributor/users/urls.py
+++ distributor/users/urls.py
@@ -1,15 +1,14 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
-from .views import *
+from .views import UsuarioViewSet
 from products.views_admin import Usuarios
 
 router = DefaultRouter()
 router.register(r'usuarios', UsuarioViewSet, basename='usuarios')
 
-
 urlpatterns = [
     # Rutas del router de DRF
-    path('api/', include(router.urls)),
+    path('', include(router.urls)),
 
     # Rutas específicas para redirigir a las plantillas HTML
 
