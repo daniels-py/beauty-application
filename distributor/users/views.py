bbcf077db21cf383e8dbd007f3f21e0de80d6327
daniels-py <danielramirezs5518@gmail.com>
2025-01-22 12:42:47 -0500

    update en end point

--- distributor/users/views.py
+++ distributor/users/views.py
@@ -1,11 +1,19 @@
-from .models import *
-from .models import CustomUser
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework import status
 from rest_framework import viewsets
-from .serializers import *
-from django.shortcuts import render
+from .models import CustomUser
+from .serializers import UsuarioSerializer
+
 
 class UsuarioViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = UsuarioSerializer
 
-
+    @action(detail=False, methods=['get'], url_path='total')
+    def total_users(self, request):
+        """
+        Método para contar el número total de usuarios.
+        """
+        user_count = CustomUser.objects.count()
+        return Response({'total_users': user_count}, status=status.HTTP_200_OK)
